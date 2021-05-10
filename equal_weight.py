9a37966c2de556a662ec9f8d1513aa646bce9179
kabigkrish <kabileshgopalakrishnan@gmail.com>
2021-05-11 03:18:13 +0530

    getting latest price of first 5 s and p stocks

--- equal_weight.py
+++ equal_weight.py
@@ -9,18 +9,24 @@ load_dotenv()
 stocks = pd.read_csv('sp_500_stocks.csv')
 
 symbol='AAPL'
-token=os.getenv('IEX_CLOUD_API_TOKEN')
-api_url = f'https://sandbox.iexapis.com/stable/stock/{symbol}/quote?token={token}'
+IEX_CLOUD_API_TOKEN=os.getenv('IEX_CLOUD_API_TOKEN')
+
+api_url = f'https://sandbox.iexapis.com/stable/stock/{symbol}/quote?token={IEX_CLOUD_API_TOKEN}'
 data = requests.get(api_url).json()
-print(data)  
+
 my_columns = ['Ticker', 'Price','Market Capitalization', 'Number Of Shares to Buy']
+
 final_dataframe = pd.DataFrame(columns = my_columns)
-final_dataframe = final_dataframe.append(
-                                        pd.Series(['AAPL', 
+for symbol in stocks['Ticker'][:5]:
+    api_url = f'https://sandbox.iexapis.com/stable/stock/{symbol}/quote?token={IEX_CLOUD_API_TOKEN}'
+    data = requests.get(api_url).json()
+    final_dataframe = final_dataframe.append(
+                                        pd.Series([symbol, 
                                                    data['latestPrice'], 
                                                    data['marketCap'], 
                                                    'N/A'], 
                                                   index = my_columns), 
                                         ignore_index = True)
+
 
 print(final_dataframe)
